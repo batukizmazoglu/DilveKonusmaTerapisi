efa41db1bfe92ad39c6064ba42f32dfd3ef7764d
batukizmazoglu <batukl89@gmail.com>
2025-02-02 23:18:28 +0200

    Vercel Analytics entegrasyonu eklendi

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { Analytics } from '@vercel/analytics/react';
 import ClientLayout from './ClientLayout';
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import CookieConsent from "@/components/CookieConsent";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,8 +18,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="tr">
       <body className={inter.className}>
-        <ClientLayout>{children}</ClientLayout>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+        <CookieConsent />
         <GoogleAnalytics gaId="G-XXXXXXXXXX" />
+        <Analytics />
       </body>
     </html>
   );
